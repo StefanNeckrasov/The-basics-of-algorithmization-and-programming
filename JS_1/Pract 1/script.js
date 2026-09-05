e0e27402d0b37de1c6e5e851fd03bcac955dd869
Stefan <132131123+StefanNeckrasov@users.noreply.github.com>
2026-09-05 18:52:26 +0300

    main

---
+++ JS_1/Pract 1/script.js
@@ -0,0 +1,12 @@
+var num = 3;
+alert(num)
+
+var a = 10;
+var b = 2;
+alert("Сумма:"+(a+b)+ " Разность: " +(a-b)+ " Произведение: " +(a*b)+ " Часное: " +(a/b));
+
+var c = 15;
+var d = 2;
+var result = c+d;
+alert(`15 + 2 =  ${result}`);
+
