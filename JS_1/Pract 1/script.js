5f47cd19d99f5a5a311ff3ba08c7081d7b23ea2f
Stefan <132131123+StefanNeckrasov@users.noreply.github.com>
2026-09-05 19:05:53 +0300

    pract 2

--- JS_1/Pract 1/script.js
+++ JS_1/Pract 1/script.js
@@ -10,3 +10,14 @@ var d = 2;
 var result = c+d;
 alert(`15 + 2 =  ${result}`);
 
+var a1 = 10;
+var b1 = 2;
+var c1 = 5;
+alert("10 + 2 + 5 = " +(a1+b1+c1))
+
+var a2 = 17;
+var b2 = 10;
+var c2 = a-b;
+var d2 = 7;
+var result1 = c2 + b2;
+alert(`17 - 10 - 7 =  ${result1}`);
