1891a1c1e0e065db11fbbacd27219f894267887b
Stefan <132131123+StefanNeckrasov@users.noreply.github.com>
2026-09-05 19:30:45 +0300

    Pract 3 and 4/2

---
+++ JS_1/Pract 2/script.js
@@ -0,0 +1,25 @@
+var srt = 'Привет, Мир!';
+alert(srt);
+
+var srt1 = 'Привет, ';
+var srt2 = 'Мир!';
+alert(srt1 + srt2);
+
+var name = 'Боря';
+alert(`Привет, ${name}!`);
+var age = 17;
+alert(`Мне ${age} лет!`);
+
+var name1 = prompt('Ваше имя: ');
+alert(`Ваше имя, ${name1}!`);
+
+var srt3 = 'abcde';
+var srt_ch;
+srt_ch = srt3[0];
+alert(srt_ch);
+srt_ch = srt3[2];
+alert(srt_ch);
+srt_ch = srt3[4];
+alert(srt_ch);
+
+var num = 12345;
