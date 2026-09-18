5b4be9d14829ba064ca49081828cc760866fceb3
Stefan <132131123+StefanNeckrasov@users.noreply.github.com>
2026-09-18 12:35:00 +0300

    update files

---
+++ JS_3/Pract 1/script.js
@@ -0,0 +1,10 @@
+var date = new Date();
+alert("Текущий День: " +date.getDate()); //дни
+alert("Текущий Месяц: " +date.getMonth()); //месяцы
+alert("Текущий Год: "+date.getFullYear()); //год
+
+
+alert(date.getMilliseconds()); //миллисекунды
+alert(date.getSeconds()); //секунды
+alert(date.getMinutes()); //минуты
+alert(date.getHours()); //часы
